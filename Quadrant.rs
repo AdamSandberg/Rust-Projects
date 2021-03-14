bf87d3655848d3da0ff39c8414e6edfad9597225
AdamSandberg <sandbergadam73@gmail.com>
2021-03-13 20:15:38 -0800

    Adds Quadrant.

---
+++ Quadrant.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+
+fn main() {
+    let mut x = String::new();
+    let mut y = String::new();
+
+println!("Type a number you would like placed in a qaudrant.");
+
+io::stdin().read_line(&mut x);
+io::stdin().read_line(&mut y);
+
+println!("the value of x is {}", x);
+println!("the value of y is {}", y);
+
+let x: i16 = x.trim().parse().expect("");
+let y: i16 = y.trim().parse().expect("");
+
+if x > 0 && y > 0 {
+    println!("1");
+}
+else if x < 0 && y > 0{
+    println!("2");
+}
+else if x < 0 && y < 0{
+    println!("3");
+}
+else {
+    println!("4");
+}
+
+}
